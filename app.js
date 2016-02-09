188785dcf521672b94c8f10c4d7672ca10af13b3
Sam Hamm <samhamm@gmail.com>
2016-02-09 11:04:12 -0800

    add index.html and app.js to repo

---
+++ app.js
@@ -0,0 +1,16 @@
+// JS file for Feb 8 class demo
+
+var userName = prompt('What is your name?');
+console.log('The user said their name is ' + userName);
+console.log(userGame);
+
+alert('Meow meow meow ' + userName + '!!!!!');
+
+var userGame = prompt('What is your game?');
+
+alert('So, ' + userName + ', I heard you like ' + userGame);
+
+var loveHate = confirm(userName + ', do you love me?');
+
+alert('Does ' + userName + ' love me? ' + loveHate);
+console.log(userGame);
